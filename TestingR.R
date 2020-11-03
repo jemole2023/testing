1ed3da279dc73a700807eedb91645a21f89e184a
jemole2023 <jnemole@gmail.com>
2020-11-02 20:14:30 -0500

    Sending from home computer in the evening 11/2

--- TestingR.R
+++ TestingR.R
@@ -3,3 +3,5 @@ print("This file was created within RStudio")
 print ("And it now ives on GitHub")
 
 print ("still working on making sure that I can push")
+
+print ("and this evening I decided to push from dowstairs home computer")
